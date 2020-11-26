3aea7a59427b07bd39336993cbbdd7f45aa04597
Florian Burger <florian.burger@mpq.mpg.de>
2020-11-26 19:29:49 +0100

    resolution added as an option in main.py

--- main.py
+++ main.py
@@ -16,23 +16,24 @@ from cavitysimulations.utilities.sweep_util import *
 
 
 def main():
+    resolution  = 30
     geom = a_poly_tapered_cavity(substrate_parameters= {'waveguide_height'  : 0.19, 'substrate_height'  : 5},
                                  waveguide_parameters = {'wvg_width' : 0.7, 'wvg_height'  : 0.19})
     
     boundary_layers = get_boundary_layer(sim2d=False)
     
-    fcen = 1/1.54
-    df = 0.2
+    fcen = 0.65
+    df = 0.01
     
     sources = [mp.Source(mp.GaussianSource(fcen, fwidth=df), 
                          component=mp.Ey, 
                          center=mp.Vector3())]
     
     #symmetries = [mp.Mirror(mp.X,+1), mp.Mirror(mp.Y,-1), mp.Mirror(mp.Z,+1)]
-   # symmetries = [mp.Mirror(mp.X,1), mp.Mirror(mp.Y,-1)]
+    #symmetries = [mp.Mirror(mp.X, +1), mp.Mirror(mp.Y,-1)]
     symmetries = [mp.Mirror(mp.X, +1)]   
     
-    sim = mp.Simulation(resolution = 30, 
+    sim = mp.Simulation(resolution = resolution, 
                         cell_size=mp.Vector3(20, 8, 8), 
                         geometry=geom, 
                         boundary_layers=boundary_layers, 
@@ -42,30 +43,25 @@ def main():
     
     h = mp.Harminv(mp.Ey, mp.Vector3(0, 0, 0), fcen, df)
     time_after_source = 500
-    
-    
-    # Don't output eps anymore to save disk space
-    
+
     #f = open("timed_harminv.txt", "w")
-    
+    sim.use_output_directory()
+
     sim.run(mp.at_beginning(mp.output_epsilon),
             mp.after_sources(h),
-            mp.when_true(lambda x: sim.round_time()%50 == 0 and sim.round_time()> 200, lambda x: print(h.modes) ),
+            mp.when_true(lambda x: sim.round_time()%50 == 0 and sim.round_time()> 200, lambda x: print("MODES: " + str([m.freq for m in h.modes])) ),
             until_after_sources=time_after_source)
     
-    #sim.run(mp.after_sources(h),
-    #	    until_after_sources = time_after_source)
     #f.close()
 
-    sim.run(mp.at_every(1/fcen/3, mp.output_efield), until=1/fcen)
+    sim.run(mp.at_every(1/fcen/3, mp.output_efield), until=1/fcen)   # if you want to create and store the field data as an h5 file
 
 #    print("--------------------------------")
 #    print([m.freq for m in h.modes])
 #    print("---------------------------------")
     
-    visualize_sim_cell(sim)
     print("Modal Volume: {}".format(sim.modal_volume_in_box()))
- 
+    #visualize_sim_cell(sim)
     
 if __name__ == '__main__':
     main()
