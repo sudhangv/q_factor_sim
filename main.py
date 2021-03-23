67dcc0dc9405a7e67ef48bdc9d16d33ae656e260
Andreas Gritsch <andreas.gritsch@mpq.mpg.de>
2021-03-23 08:58:37 +0100

    Modify main to one in qs

--- main.py
+++ main.py
@@ -17,21 +17,24 @@ from cavitysimulations.utilities.sweep_util import *
 
 def main():
     
-    wvg_height = 0.19
-    
+    wvg_height = 0.1
+    resolution = 40
+    sx, sy, sz =  20, 8, 8
+
     geom = a_poly_tapered_cavity(substrate_parameters= {'wvg_height': wvg_height, 'substrate_height': 5},
                                  waveguide_parameters = {'wvg_width': 0.7, 'wvg_height'  : wvg_height},
-                                 cavity_parameters = {'hx' : 0.3, 'hy': 0.3, 'w' : 0.7, 'wvg_height': wvg_height,
-                                                      'a_center': 0.379, 'a_mirror': 0.424, 
-                                                      'Lx': 20, 'number_of_tapered_holes': 7,
+                                 cavity_parameters = {'hx' : 0.175, 'hy': 0.325, 'w' : 0.7, 'wvg_height': wvg_height,
+                                                      'a_center': 0.429, 'a_mirror': 0.449, 
+                                                      'Lx': 20, 'number_of_tapered_holes': 10,
                                                       'substrate': True, 'mode': 'yO' },
-                                 sweep_parameters = {'hx_min_sweep': 0.15, 'hy_min_sweep': 0.225},
-                                 filename = "bandstructure_data/perturb_sub_190_yO.h5")
+                                 sweep_parameters = {'hx_min_sweep': 0.05, 'hy_min_sweep': 0.05},
+                                 filename = "bandstructure_data/perturb_sub_100_yO.hdf5")
     
+
     boundary_layers = get_boundary_layer(sim2d=False)
     
-    fcen = 0.6429
-    df = 0.1
+    fcen = 0.6412
+    df = 0.005
     
     sources = [mp.Source(mp.GaussianSource(fcen, fwidth=df), 
                          component=mp.Ey, 
@@ -41,8 +44,8 @@ def main():
     #symmetries = [mp.Mirror(mp.X, +1), mp.Mirror(mp.Y,-1)]
     symmetries = [mp.Mirror(mp.X, +1)]   
     
-    sim = mp.Simulation(resolution = 30, 
-                        cell_size = mp.Vector3(20, 8, 8), 
+    sim = mp.Simulation(resolution = resolution, 
+                        cell_size = mp.Vector3(sx, sy, sz), 
                         geometry = geom, 
                         boundary_layers = boundary_layers, 
                         sources = sources,
